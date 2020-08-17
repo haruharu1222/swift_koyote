e7be655b36de1040f317d5c84e943daf769ba403
haruhi.yamaguchi <1te16203p@s.kyushu-u.ac.jp>
2020-08-17 21:43:28 +0900

    加える

---
+++ PreviousGameController.swift
@@ -0,0 +1,36 @@
+//
+//  PreviousGameController.swift
+//  otamesi
+//
+//  Created by 山口遥陽 on 2020/08/15.
+//  Copyright © 2020 山口遥陽. All rights reserved.
+//
+
+import UIKit
+
+class PreviousGameController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    
+    @IBAction func start(_ sender: Any) {
+    }
+    
+
+}
